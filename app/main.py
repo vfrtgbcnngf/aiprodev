393cb4d9886ab9e6f28cfa3ac3931534d6a831ba
vfrtgbcnngf <shalldow73@gmail.com>
2026-07-24 16:31:32 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -16,10 +16,10 @@ app.mount("/static", StaticFiles(directory=static_dir), name="static")
 templates_dir = os.path.join(os.path.dirname(__file__), "templates")
 templates = Jinja2Templates(directory=templates_dir)
 
-# 루트('/') 접속 시 index.html을 화면에 렌더링하는 코드 추가
+# 루트('/') 접속 시 index.html을 화면에 렌더링 (최신 Starlette 규격에 맞게 수정)
 @app.get("/")
 def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request, "index.html", {"request": request})
 
 # 라우터 등록
 app.include_router(items.router)
